98887d1217d3dfa919f30023b93a1f0c01fc1bb8
beanstown106 <nbenis106@gmail.com>
2020-09-28 04:41:17 +0000

    BaseCamp: Fingerprint authentication vibration [2/2]
    Change-Id: Id8617ad498ea91da6b563e34f6b2e124a5162e63
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/com/base/camp/categories/LockScreenSettings.java
+++ src/com/base/camp/categories/LockScreenSettings.java
@@ -24,6 +24,7 @@ import android.content.ContentResolver;
 import android.app.WallpaperManager;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -54,6 +55,13 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
     	private SwitchPreference mKGCustomClockColor;
     	private SecureSettingSwitchPreference mDoubleLineClock;
 
+    private static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+    private static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
+
+    private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFingerprintSuccessVib;
+    private SwitchPreference mFingerprintErrorVib;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -63,6 +71,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         Resources resources = getResources();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final PreferenceScreen prefSet = getPreferenceScreen();
+        final PackageManager mPm = getActivity().getPackageManager();
         final PreferenceCategory ambientCat = (PreferenceCategory) prefScreen.findPreference(CATEGORY_AMBIENT);
         if (TextUtils.isEmpty(getResources().getString(com.android.internal.R.string.config_dozeDoubleTapSensorType)) &&
                 TextUtils.isEmpty(getResources().getString(com.android.internal.R.string.config_dozeTapSensorType))) {
@@ -73,6 +82,27 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         mDoubleLineClock.setChecked((Settings.Secure.getInt(getContentResolver(),
              Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK, 1) != 0));
         mDoubleLineClock.setOnPreferenceChangeListener(this);
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintSuccessVib = (SwitchPreference) findPreference(FINGERPRINT_SUCCESS_VIB);
+        mFingerprintErrorVib = (SwitchPreference) findPreference(FINGERPRINT_ERROR_VIB);
+        if (mPm.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) &&
+                mFingerprintManager != null) {
+            if (!mFingerprintManager.isHardwareDetected()) {
+                prefSet.removePreference(mFingerprintSuccessVib);
+                prefSet.removePreference(mFingerprintErrorVib);
+            } else {
+                mFingerprintSuccessVib.setChecked((Settings.System.getInt(getContentResolver(),
+                        Settings.System.FP_SUCCESS_VIBRATE, 1) == 1));
+                mFingerprintSuccessVib.setOnPreferenceChangeListener(this);
+                mFingerprintErrorVib.setChecked((Settings.System.getInt(getContentResolver(),
+                        Settings.System.FP_ERROR_VIBRATE, 1) == 1));
+                mFingerprintErrorVib.setOnPreferenceChangeListener(this);
+            }
+        } else {
+            prefSet.removePreference(mFingerprintSuccessVib);
+            prefSet.removePreference(mFingerprintErrorVib);
+        }
 
         mBlurRadiusPref = (Preference) findPreference(
                         BLUR_RADIUS_KEY);
@@ -91,11 +121,21 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
 
-            if (preference == mDoubleLineClock) {
+        if (preference == mDoubleLineClock) {
         	boolean value = (Boolean) newValue;
             Settings.Secure.putInt(resolver,
                     Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK, value ? 1 : 0);
-            return true; 
+            return true;
+        } else if (preference == mFingerprintSuccessVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FP_SUCCESS_VIBRATE, value ? 1 : 0);
+            return true;
+        } else if (preference == mFingerprintErrorVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.FP_ERROR_VIBRATE, value ? 1 : 0);
+            return true;
         }
         return false;
     }
